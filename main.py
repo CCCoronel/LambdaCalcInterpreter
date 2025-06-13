ce4066940b3cdcb9463cefd5f430ce25c1375ab4
Caio Cesar Oliva Coronel <caio.coronel@estudante.ifes.edu.br>
2025-06-13 18:03:29 -0300

    Teste de Tokenizer

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-import re
+from lambda_io.file_loader import load_definition
+from parser.tokenizer import tokenize_lambda_expression, print_tokens
 
 def main():
+    # Carregar definições de um arquivo
+    try:
+        bindings = load_definition("./lambda.bnf")  # Ou ajuste o caminho conforme necessário
+    except FileNotFoundError:
+        print("Arquivo lambda.bnf não encontrado.")
+        return
+    except SyntaxError as e:
+        print(f"Erro de sintaxe no arquivo: {e}")
+        return
+
+    print("\n==== Tokens de cada definição carregada ====\n")
+
+    for name, expr in bindings:
+        print(f"\n--- Tokenizando a definição: {name} ---")
+        print(f"Expressão: {expr}")
+
+        tokens = tokenize_lambda_expression(expr)
+        print_tokens(tokens)
+
+    print (f"--------- TESTE BASEADO EM UM CODIGO ---------")
+
     code = "(λx. x)"
-    tokens = tokenize(code)
+    tokens = tokenize_lambda_expression(code)
+    print(f"Codigo: {code}\n")
     for t in tokens:
         print(t)
 
+
 if __name__ == "__main__":
     main()
-
