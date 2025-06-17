85f0cd3fdc50105f4b1936f2f79f741c314d90b8
Caio Cesar Oliva Coronel <160687497+CCCoronel@users.noreply.github.com>
2025-06-17 18:46:20 -0300

    Comentarios.py

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from evaluator.evaluator import evaluate
 from ast_nodes.nodes import Binding
 
 def main():
+    #Aqui ele cria o ambiente
     env = Environment()
     # Carrega todas as definições do core.lambda
     try:
+        #Cria uma lista de tublas com as definições. As tuplas são divididas em (name, expr) sendo name o nome da expressão (id, true, false, etc) e o expr a expressão lambda correspondente 
         bindings = load_definition("core.lambda")
     except FileNotFoundError:
         print("Arquivo core.lambda não encontrado.")
@@ -16,15 +18,19 @@ def main():
     except SyntaxError as e:
         print(f"Erro de sintaxe ao carregar definições: {e}")
         return
-
+    
     # Parseia cada binding
     for name, expr_str in bindings:
+        #Separa em tokens cada um dos elementos da expressão
         tokens = tokenize_lambda_expression(expr_str)
+        #Usa esses tokens para fazer o pareamento e recebe uma lista de estado
         ast_list = parse_tokens(tokens)
+        #Print de teste
         print(ast_list)
+        #Confere cada um dos estados dos nós 
         for stmt in ast_list:
             if isinstance(stmt, Binding):
-                # Impossível, pois no core só há expressões (não statements soltos)
+                # Impossível, pois no core só há expressões (não statements soltos) (calma que é possivel sim)
                 env.define(stmt.name, stmt.expr)
             else:
                 env.define(name, stmt)
@@ -32,7 +38,7 @@ def main():
     print("\n=== Ambiente carregado com sucesso! ===")
     print(f"Bindings definidos: {list(env.bindings.keys())}")
 
-    # Agora vamos testar uma expressão simples
+    # Agora vamos testar uma expressão simples (Aqui ta dando erro ainda)
     test_expr = "(and true false)"
 
     print(f"\n=== Testando a expressão: {test_expr} ===")
